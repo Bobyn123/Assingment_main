035b2ea78add9cb935577107f82f8172af4feed5
Bobyn123 <55721221+Bobyn123@users.noreply.github.com>
2020-11-26 10:17:38 +0000

    Completed basic file loading system, takes input and ignores garbage data, enters input into linked list.
    currently working on stopping duplicate data being entered into list

---
+++ distances.c
@@ -0,0 +1,40 @@
+//
+// Created by Bobyn on 14/11/2020.
+//
+
+
+#include <math.h>
+#include "functions.h"
+#define degToRad(angleInDegrees) ((angleInDegrees) * M_PI / 180.0)
+#define radToDeg(angleInRadians) ((angleInRadians) * 180.0 / M_PI)
+
+
+/**
+	 * The great circle distance or the orthodromic distance is the shortest distance
+	 * between two points on a sphere
+	 * @param other
+	 * @return distance in m
+	 */
+double distance(sheep_position * first, sheep_position * second)
+{
+    double lon1 = degToRad(first->longitude);
+    double lon2 = degToRad(second->longitude);
+    double lat1 = degToRad(first->latitude);
+    double lat2 = degToRad(second->latitude);
+
+    // Haversine formula (alt. vincenties formula)
+    double dlon = lon2 - lon1;
+    double dlat = lat2 - lat1;
+    double a = pow(sin(dlat / 2), 2)
+               + cos(lat1) * cos(lat2)
+                 * pow(sin(dlon / 2),2);
+
+    double c = 2 * asin(sqrt(a));
+
+    // Radius of earth in kilometers. Use 3956
+    // for miles
+    double r = 6371;
+
+    // calculate the result (in m)
+    return((c * r)*1000);
+}
--- functions.h
+++ functions.h
@@ -16,4 +16,5 @@ node_pointer make_sheep_node(double new_latitude, double new_longitude, struct t
 void insert_at_top(node_pointer *ptr_to_head, node_pointer new_sheep_ptr);
 void insert_at_tail(node_pointer *ptr_to_head, node_pointer new_sheep_ptr);
 int is_duplicate_node(node_pointer new_sheep, node_pointer current_sheep);
+double distance(sheep_position * first, sheep_position * second);
 #endif //ASSINGMENT_MAIN_FUNCTIONS_H
